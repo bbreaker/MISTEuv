a368612cde3748bcb4c2536acfe737b28f4a9f92
Brian Breaker <bbreaker@usgs.gov>
2017-04-10 14:49:25 -0500

    Update global.R

--- inst/shiny/global.R
+++ inst/shiny/global.R
@@ -12,15 +12,3 @@ library(gam)
 library(mgcv)
 
 options(DT.options = list(pageLength = 7, language = list(search = 'Filter:')))
-
-source("MISTEuv/R/getRegDat.R")
-
-source("MISTEuv/R/getEstDat.R")
-
-source("MISTEuv/R/getBestOffset.R")
-
-source("MISTEuv/R/allMISTEdat.R")
-
-source("MISTEuv/R/applySmooth.R")
-
-source("MISTEuv/R/MISTEuvGUI.R")
