9822097fb2f91a29e03a6c67bbd9ba5c5cf34c48
Brian Breaker <bbreaker@usgs.gov>
2017-04-10 15:44:52 -0500

    Add files via upload

---
+++ inst/shiny/plotEvsO.R
@@ -0,0 +1,18 @@
+output$plotEvsO <- renderPlot({
+  
+  estDat <- getEstDat()
+  
+  regDat <- getRegDat()
+  
+  datP <- allMISTEdat(estDat, regDat)
+  
+  p <- ggplot(data = datP, aes(x = Flow.y, y = Estimated)) +
+    geom_point(size = 3) +
+    stat_smooth(method = "lm") + 
+    labs(x = "Observed discharge, in cubic feet per second", 
+         y = "Estimated discharge, in cubic feet per second") +
+    theme_bw() + theme(legend.title = element_blank())
+  
+  p
+  
+})
---
+++ inst/shiny/regCust.R
@@ -0,0 +1,66 @@
+output$regCust <- renderPrint({
+  
+  estDat <- getEstDat()
+  
+  regDat <- getRegDat()
+  
+  datP <- allMISTEdat(estDat, regDat)
+  
+  if (input$use2 == FALSE) {
+    
+    if (input$Method == 1) {
+      
+      regObj <- lm(log10(Flow.y) ~ log10(Flow.x), data = regDat)
+      
+      newSummary <- list()
+      
+      newSummary[[1]] <- paste0("Index site ", input$xID, " was used to estimate data for ", input$yID)
+      
+      newSummary
+      
+    }
+    
+    else if (input$Method == 2) {
+      
+      regObj <- gam(log10(Flow.y) ~ s(log10(Flow.x), bs = "ts"), data = regDat, select = TRUE)
+      
+      newSummary <- list()
+      
+      newSummary[[1]] <- paste0("Index site ", input$xID, " was used to estimate data for ", input$yID)
+      
+      newSummary
+      
+    }
+    
+  }
+  
+  else if (input$use2 == TRUE) {
+    
+    if (input$Method == 1) {
+      
+      regObj <- lm(log10(Flow.y) ~ log10(Flow.x) + log10(Flow.x2), data = regDat)
+      
+      newSummary <- list()
+      
+      newSummary[[1]] <- paste0("Index sites ", input$xID, " and ", input$xID2, " was used to estimate data for ", input$yID)
+      
+      newSummary
+      
+    }
+    
+    else if (input$Method == 2) {
+      
+      regObj <- gam(log10(Flow.y) ~ s(log10(Flow.x), bs = "ts") + 
+                      s(log10(Flow.x2), bs = "ts"), data = regDat, select = TRUE)
+      
+      newSummary <- list()
+      
+      newSummary[[1]] <- paste0("Index sites ", input$xID, " and ", input$xID2, " was used to estimate data for ", input$yID)
+      
+      newSummary
+      
+    }
+    
+  }
+  
+})
