872418fecd805c3de88075e7acde59f0b22de191
Brian Breaker <bbreaker@usgs.gov>
2017-04-11 14:30:56 -0500

    Add files via upload

---
+++ inst/rmarkdown/README.md
@@ -0,0 +1,41 @@
+README
+================
+
+MISTEuv (Beta)
+--------------
+
+MISTEuv was developed to estimate missing unit-value streamflow data for USGS streamflow-gaging stations. Estimates are generated using a generalized additive model (GAM) with one or two index streamflow gaging stations.
+
+Software
+--------
+
+If R (version 3.3 or greater) is not already installed on your computer, download and install the latest binary distribution from the Comprehensive R Archive Network ([CRAN](https://cloud.r-project.org/)). R can also be run from RStudio, which is a nice interface for working with R and provides a console. In order to use RStudio, R must be installed first. The latest version of RStudio is available [here](https://www.rstudio.com/products/rstudio/download/). R and RStudio should be accessible from the start menu, a desktop shortcut, or via command shell. Assuming you installed R on a 64-bit operating system (OS), two versions of R are made available, that is, a 32-bit and 64-bit version. The 64-bit version of R (R x64) is preferred. In order to add the USGS Geological Survey R Archive Network, or the [GRAN](https://owi.usgs.gov/R/gran.html), as a repository from which to download USGS R packages;
+
+``` r
+rprofile_path = file.path(Sys.getenv("HOME"), ".Rprofile")
+write('\noptions(repos=c(getOption(\'repos\'),
+    CRAN=\'https://cloud.r-project.org\',
+    USGS=\'https://owi.usgs.gov/R\'))\n',
+      rprofile_path, 
+      append =  TRUE)
+```
+
+Installation
+------------
+
+To install and launch MISTEuv;
+
+``` r
+install.packages("devtools")
+
+devtools::install_github("bbreaker-USGS/MISTEuv")
+
+MISTEuv::MISTEuvgui()
+```
+
+Disclaimer
+----------
+
+Software created by USGS employees along with contractors and grantees (unless specific stipulations are made in a contract or grant award) are to be released as Public Domain and free of copyright or license. Contributions of software components such as specific algorithms to existing software licensed through a third party are encouraged, but those contributions should be annotated as freely available in the Public Domain wherever possible. If USGS software uses existing licensed components, those licenses must be adhered to and redistributed.
+
+Although this software has been used by the U.S. Geological Survey (USGS), no warranty, expressed or implied, is made by the USGS or the U.S. Government as to accuracy and functionality, nor shall the fact of distribution constitute any such warranty, and no responsibility is assumed by the USGS in connection therewith.
---
+++ inst/shiny/plotComp1.R
@@ -0,0 +1,67 @@
+output$plotComp1 <- renderPlot({
+  
+  regDat <- getRegDat()
+  
+  bestOffsetVal <- getBestOffset(regDat) 
+  
+  if(input$lag1 == 0) {
+    
+    if (input$log10 == FALSE) {
+      
+      p <- ggplot() +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID))) +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), linetype = "dotdash") +
+        annotate("text", label = paste0("calculated best time offset = ", bestOffsetVal, " minutes"), 
+                 y = 0.99*(max(regDat$Flow.x)), x = mean(regDat$dateTime)) + 
+        labs(x = "Date", y = "Discharge, in cubic feet per second") +
+        theme_bw() + theme(legend.title = element_blank())
+      
+    }
+    
+    else if (input$log10 == TRUE) {
+      
+      p <- ggplot() +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID))) +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), linetype = "dotdash") +
+        scale_y_log10() +
+        annotate("text", label = paste0("calculated best time offset = ", bestOffsetVal, " minutes"),
+                 y = 0.99*(max(regDat$Flow.x)), x = mean(regDat$dateTime)) + 
+        annotation_logticks(sides = "rl") +
+        labs(x = "Date", y = "Discharge, in cubic feet per second") +
+        theme_bw() + theme(legend.title = element_blank(), panel.grid.minor = element_blank())
+      
+    }
+    
+    p
+    
+  }
+  
+  else if(input$lag1 != 0) {
+    
+    if (input$log10 == FALSE) {
+      
+      p <- ggplot() +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID))) +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), linetype = "dotdash") +
+        labs(x = "Date", y = "Discharge, in cubic feet per second") +
+        theme_bw() + theme(legend.title = element_blank())
+      
+    }
+    
+    else if (input$log10 == TRUE) {
+      
+      p <- ggplot() +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID))) +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), linetype = "dotdash") +
+        scale_y_log10() +
+        annotation_logticks(sides = "rl") +
+        labs(x = "Date", y = "Discharge, in cubic feet per second") +
+        theme_bw() + theme(legend.title = element_blank(), panel.grid.minor = element_blank())
+      
+    }
+    
+    p
+    
+  }
+  
+})
