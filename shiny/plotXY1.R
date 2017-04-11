c1c66c44fc4a6b72a9e7dedb13b616a7b7dcf4c6
Brian Breaker <bbreaker@usgs.gov>
2017-04-11 14:25:34 -0500

    Add files via upload

---
+++ shiny/plotXY1.R
@@ -0,0 +1,41 @@
+output$plotXY1 <- renderPlot({
+  
+  estDat <- getEstDat()
+  
+  regDat <- getRegDat()
+  
+  datP <- allMISTEdat(estDat, regDat)
+  
+  if (input$Method == 1) {
+    
+    p <- ggplot(data = datP, aes(x = Flow.x, y = Flow.y)) +
+      geom_point(size = 3) +
+      scale_y_log10() +
+      scale_x_log10() +
+      annotation_logticks(sides = "trbl") +
+      stat_smooth(method = "lm") + 
+      labs(x = paste0("Discharge, in cubic feet per second \n for ", input$xID), 
+           y = paste0("Discharge, in cubic feet per second \n for ", input$yID)) + 
+      theme_bw() + theme(legend.title = element_blank())
+    
+    p
+    
+  }
+  
+  else if (input$Method == 2) {
+    
+    p <- ggplot(data = datP, aes(x = Flow.x, y = Flow.y)) +
+      geom_point(size = 3) +
+      scale_y_log10() +
+      scale_x_log10() +
+      annotation_logticks(sides = "trbl") +
+      stat_smooth(method = "gam", formula = y ~ s(x, bs = "ts")) + 
+      labs(x = paste0("Discharge, in cubic feet per second \n for ", input$xID), 
+           y = paste0("Discharge, in cubic feet per second \n for ", input$yID)) + 
+      theme_bw() + theme(legend.title = element_blank())
+    
+    p
+    
+  }
+  
+})
