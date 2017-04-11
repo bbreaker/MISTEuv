1cbda396236ed7c3495c3ce7a54b671c0cf792de
Brian Breaker <bbreaker@usgs.gov>
2017-04-11 13:01:11 -0500

    Add files via upload

--- inst/shiny/getDataFile.R
+++ inst/shiny/getDataFile.R
@@ -10,6 +10,22 @@ output$downloadData <- downloadHandler(
     
     datP <- allMISTEdat(estDat, regDat)
     
+    if(input$smooth == FALSE) {
+      
+      datP <- datP %>%
+        dplyr::filter(is.na(Flow.y)) %>%
+        dplyr::select(dateTime, Flow = Estimated)
+      
+    }
+    
+    else if(input$smooth == TRUE) {
+      
+      datP <- datP %>%
+        dplyr::filter(is.na(Flow.y)) %>%
+        dplyr::select(dateTime, Flow = Smoothed)
+      
+    }
+    
     write.csv(datP, file, row.names = FALSE)
     
   }
