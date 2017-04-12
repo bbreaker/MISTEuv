45a829d982b1932db301c19a50dd22ba28ab0d4e
Brian Breaker <bbreaker@usgs.gov>
2017-04-12 07:34:02 -0500

    Add files via upload

--- inst/shiny/plotComp1.R
+++ inst/shiny/plotComp1.R
@@ -25,7 +25,7 @@ output$plotComp1 <- renderPlot({
         geom_line(data = regDat, aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), linetype = "dotdash") +
         scale_y_log10() +
         annotate("text", label = paste0("calculated best time offset = ", bestOffsetVal, " minutes"),
-                 y = 0.99*(max(regDat$Flow.x)), x = x = as.POSIXct(regDat[nrow(regDat)/2,1], tz = "GMT")) + 
+                 y = 0.99*(max(regDat$Flow.x)), x = as.POSIXct(regDat[nrow(regDat)/2,1], tz = "GMT")) + 
         annotation_logticks(sides = "rl") +
         labs(x = "Date", y = "Discharge, in cubic feet per second") +
         theme_bw() + theme(legend.title = element_blank(), panel.grid.minor = element_blank())
