5e3d851c10b1767b8ac778cea59e9b56269392b6
Brian Breaker <bbreaker@usgs.gov>
2017-04-13 15:09:36 -0500

    Add files via upload

---
+++ inst/shiny/plotComp1.R
@@ -0,0 +1,67 @@
+output$plotComp1 <- renderPlot({
+  
+  regDat <- getRegDat()
+  
+  bestOffsetVal <- getBestOffset(regDat) 
+  
+  if(input$lag1 == 0) {
+    
+    if (input$log10 == FALSE) {
+      
+      p <- ggplot() +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID))) +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), linetype = "dotdash") +
+        annotate("text", label = paste0("calculated best time offset = ", bestOffsetVal, " minutes"), 
+                 x = as.POSIXct(-Inf, origin = '1970-01-01'), y = 0.99*max(regDat$Flow.x)) + 
+        labs(x = "Date", y = "Discharge, in cubic feet per second") +
+        theme_bw() + theme(legend.title = element_blank())
+      
+    }
+    
+    else if (input$log10 == TRUE) {
+      
+      p <- ggplot() +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID))) +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), linetype = "dotdash") +
+        scale_y_log10() +
+        annotate("text", label = paste0("calculated best time offset = ", bestOffsetVal, " minutes"),
+                 x = as.POSIXct(mean(regDat$dateTime, origin = '1970-01-01')), y = 0.99*max(regDat$Flow.x)) + 
+        annotation_logticks(sides = "rl") +
+        labs(x = "Date", y = "Discharge, in cubic feet per second") +
+        theme_bw() + theme(legend.title = element_blank(), panel.grid.minor = element_blank())
+      
+    }
+    
+    p
+    
+  }
+  
+  else if(input$lag1 != 0) {
+    
+    if (input$log10 == FALSE) {
+      
+      p <- ggplot() +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID))) +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), linetype = "dotdash") +
+        labs(x = "Date", y = "Discharge, in cubic feet per second") +
+        theme_bw() + theme(legend.title = element_blank())
+      
+    }
+    
+    else if (input$log10 == TRUE) {
+      
+      p <- ggplot() +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID))) +
+        geom_line(data = regDat, aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), linetype = "dotdash") +
+        scale_y_log10() +
+        annotation_logticks(sides = "rl") +
+        labs(x = "Date", y = "Discharge, in cubic feet per second") +
+        theme_bw() + theme(legend.title = element_blank(), panel.grid.minor = element_blank())
+      
+    }
+    
+    p
+    
+  }
+  
+})
