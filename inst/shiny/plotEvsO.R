b07a36b8b57209a905cfb6804404f7eb6094b5c9
Brian Breaker <bbreaker@users.noreply.github.com>
2017-06-30 11:41:15 -0500

    Add files via upload

---
+++ inst/shiny/plotEvsO.R
@@ -0,0 +1,20 @@
+output$plotEvsO <- renderPlot({
+  
+  estDat <- getEstDat()
+  
+  regDat <- getRegDat()
+  
+  datP <- allMISTEdat(estDat, regDat)
+  
+  p <- ggplot(data = datP, aes(x = Flow.y, y = Estimated)) +
+    geom_point(size = 3) +
+    scale_y_continuous(limits = c(0,max(datP$Flow.y)), labels = comma) +
+    scale_x_continuous(limits = c(0,max(datP$Flow.y)), labels = comma) +
+    stat_smooth(method = "lm", se = FALSE) + 
+    labs(x = "Observed discharge, in cubic feet per second", 
+         y = "Estimated discharge, in cubic feet per second") +
+    theme_bw() + theme(legend.title = element_blank())
+  
+  p
+  
+})
