e9c5968318e6228957c9f1d7454171c59e415ac7
bbreaker <bbreaker@usgs.gov>
2017-08-04 11:48:12 -0500

    updated smooth function

--- R/smoothAddQ.R
+++ R/smoothAddQ.R
@@ -117,9 +117,9 @@ smoothAddQ <- function(df, ObsDf, startSm, endSm, startDat, endDat) {
       
     }
     
-    smPeriodAdj <- subset(df, dateTime >= startDat & dateTime <= endDat)
+    smPeriodAdj <- seq(from = as.POSIXct(startDat, tz = "GMT"), to = as.POSIXct(endDat, tz = "GMT"), by = "15 mins")
     
-    adjResidAddQs <- data.frame(dateTime = smPeriodAdj$dateTime,
+    adjResidAddQs <- data.frame(dateTime = smPeriodAdj,
                                 adjResid = unlist(adjResidList))
     
     smPeriod <- dplyr::left_join(x = smPeriod, y = adjResidAddQs, by = "dateTime")
