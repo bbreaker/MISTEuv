6112ca4252f21f92cf70272adf68171de9d72268
bbreaker <bbreaker@usgs.gov>
2017-08-07 11:07:29 -0500

    updated E vs O plot

--- inst/shiny/plotEvsO.R
+++ inst/shiny/plotEvsO.R
@@ -10,8 +10,8 @@ output$plotEvsO <- renderPlot({
   
   p <- ggplot(data = datP, aes(x = Flow.y, y = Estimated)) +
     geom_point(size = 3) +
-    scale_y_continuous(limits = c(0,max(forPlot$Flow.x)), labels = comma) +
-    scale_x_continuous(limits = c(0,max(forPlot$Flow.x)), labels = comma) +
+    scale_y_continuous(limits = c(0,max(forPlot$Estimated)), labels = comma) +
+    scale_x_continuous(limits = c(0,max(forPlot$Estimated)), labels = comma) +
     stat_smooth(method = "lm", se = FALSE) + 
     labs(x = "Observed discharge, in cubic feet per second", 
          y = "Estimated discharge, in cubic feet per second") +
