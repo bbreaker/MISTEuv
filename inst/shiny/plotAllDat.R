ffb6e4fc291d69d67f18ab55803ffcc51ddebe8c
bbreaker <bbreaker@usgs.gov>
2017-09-19 11:54:59 -0500

    updated graphics

--- inst/shiny/plotAllDat.R
+++ inst/shiny/plotAllDat.R
@@ -13,16 +13,19 @@ output$plotAllDat <- renderPlot({
       if (input$log10 == FALSE) {
         
         p <- ggplot() +
+          geom_line(data = datP[!is.na(datP$Flow.y),], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_rect(aes(xmin = as.POSIXct(input$estDateSt, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         xmax = as.POSIXct(input$estDateEn, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         ymin = -Inf, ymax = Inf), alpha = 0.4, color = "grey80") +
-          geom_line(data = datP[!is.na(datP$Flow.y),], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_line(data = datP[!is.na(datP$Flow.x),], aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), size = 1, linetype = "dotdash") +
           geom_line(data = datP[!is.na(datP$Estimated),], aes(x = dateTime, y = Estimated, color = "Estimated"), size = 1, linetype = "longdash") +
           geom_line(data = datP[!is.na(datP$fitUpper),], aes(x = dateTime, y = fitUpper), color = "black", size = 1, linetype = "dashed") +
           geom_line(data = datP[!is.na(datP$fitLower),], aes(x = dateTime, y = fitLower), color = "black", size = 1, linetype = "dashed") +
           scale_y_continuous(labels = comma) +
           labs(x = "Date (UTC)", y = "Discharge, in cubic feet per second") +
+          scale_color_manual(values = c(paste0("y-", input$yID) = "blue", 
+                             paste0("x-", input$xID) = "green", 
+                             "Estimated" = "red")) +
           theme_bw() + theme(legend.title = element_blank())
         
       }	
@@ -30,10 +33,10 @@ output$plotAllDat <- renderPlot({
       else if (input$log10 == TRUE) {
         
         p <- ggplot() +
+          geom_line(data = datP[!is.na(datP$Flow.y),], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_rect(aes(xmin = as.POSIXct(input$estDateSt, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         xmax = as.POSIXct(input$estDateEn, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         ymin = 0, ymax = Inf), alpha = 0.4, color = "grey80") +
-          geom_line(data = datP[!is.na(datP$Flow.y),], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_line(data = datP[!is.na(datP$Flow.x),], aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), size = 1, linetype = "dotdash") +
           geom_line(data = datP[!is.na(datP$Estimated),], aes(x = dateTime, y = Estimated, color = "Estimated"), size = 1, linetype = "longdash") +
           geom_line(data = datP[!is.na(datP$fitUpper),], aes(x = dateTime, y = fitUpper), color = "black", size = 1, linetype = "dashed") +
@@ -41,6 +44,9 @@ output$plotAllDat <- renderPlot({
           scale_y_log10(labels = comma) +
           annotation_logticks(sides = "rl") +
           labs(x = "Date (UTC)", y = "Discharge, in cubic feet per second") +
+          scale_color_manual(values = c(paste0("y-", input$yID) = "blue", 
+                                        paste0("x-", input$xID) = "green", 
+                                        "Estimated" = "red")) +
           theme_bw() + theme(legend.title = element_blank(), panel.grid.minor = element_blank())
         
       }
@@ -54,17 +60,21 @@ output$plotAllDat <- renderPlot({
       if (input$log10 == FALSE) {
         
         p <- ggplot() +
+          geom_line(data = datP[!is.na(datP$Flow.y)], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_rect(aes(xmin = as.POSIXct(input$estDateSt, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         xmax = as.POSIXct(input$estDateEn, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         ymin = -Inf, ymax = Inf), alpha = 0.4, color = "grey80") +
           geom_line(data = datP[!is.na(datP$Flow.x)], aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), linetype = "dotdash") +
           geom_line(data = datP[!is.na(datP$Flow.x2)], aes(x = dateTime, y = Flow.x2, color = paste0("x2-", input$xID2)), size = 1, linetype = "dotdash") +
-          geom_line(data = datP[!is.na(datP$Flow.y)], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_line(data = datP[!is.na(datP$Estimated)], aes(x = dateTime, y = Estimated, color = "Estimated"), size = 1, linetype = "longdash") +
           geom_line(data = datP[!is.na(datP$fitUpper)], aes(x = dateTime, y = fitUpper), color = "black", size = 1, linetype = "dashed") +
           geom_line(data = datP[!is.na(datP$fitLower)], aes(x = dateTime, y = fitLower), color = "black", size = 1, linetype = "dashed") +
           scale_y_continuous(labels = comma) +
           labs(x = "Date (UTC)", y = "Discharge, in cubic feet per second") +
+          scale_color_manual(values = c(paste0("y-", input$yID) = "blue", 
+                                        paste0("x-", input$xID) = "green",
+                                        paste0("x2-", input$xID2) = "purple",
+                                        "Estimated" = "red")) +
           theme_bw() + theme(legend.title = element_blank())
         
       }
@@ -72,18 +82,22 @@ output$plotAllDat <- renderPlot({
       else if (input$log10 == TRUE) {
         
         p <- ggplot() +
+          geom_line(data = datP[!is.na(datP$Flow.y)], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_rect(aes(xmin = as.POSIXct(input$estDateSt, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         xmax = as.POSIXct(input$estDateEn, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         ymin = 0, ymax = Inf), alpha = 0.4, color = "grey80") +
           geom_line(data = datP[!is.na(datP$Flow.x)], aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), size = 1, linetype = "dotdash") +
           geom_line(data = datP[!is.na(datP$Flow.x2)], aes(x = dateTime, y = Flow.x2, color = paste0("x2-", input$xID2)), size = 1, linetype = "dotdash") +
-          geom_line(data = datP[!is.na(datP$Flow.y)], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_line(data = datP[!is.na(datP$Estimated)], aes(x = dateTime, y = Estimated, color = "Estimated"), size = 1, linetype = "longdash") +
           geom_line(data = datP[!is.na(datP$fitUpper)], aes(x = dateTime, y = fitUpper), color = "black", size = 1, linetype = "dashed") +
           geom_line(data = datP[!is.na(datP$fitLower)], aes(x = dateTime, y = fitLower), color = "black", size = 1, linetype = "dashed") +
           scale_y_log10(labels = comma) +
           annotation_logticks(sides = "rl") +
           labs(x = "Date (UTC)", y = "Discharge, in cubic feet per second") +
+          scale_color_manual(values = c(paste0("y-", input$yID) = "blue", 
+                                        paste0("x-", input$xID) = "green",
+                                        paste0("x2-", input$xID2) = "purple",
+                                        "Estimated" = "red")) +
           theme_bw() + theme(legend.title = element_blank(), panel.grid.minor = element_blank())
         
       }
@@ -98,20 +112,32 @@ output$plotAllDat <- renderPlot({
     
     if (input$giveObsQ == TRUE) {
       
+      addQs <- as.numeric(unlist(strsplit(input$ObsQ, ", ")))
+      
+      addDTs <- unlist(strsplit(input$ObsQDate, ", "))
+      
+      ObsDf <<- data.frame(dateTime = as.POSIXct(addDTs, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"),
+                           Flow.obs = addQs)
+      
       if (input$log10 == FALSE) {
         
         p <- ggplot() +
+          geom_line(data = datP[!is.na(datP$Flow.y),], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_rect(aes(xmin = as.POSIXct(input$estDateSt, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         xmax = as.POSIXct(input$estDateEn, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         ymin = -Inf, ymax = Inf), alpha = 0.4, color = "grey80") +
-          geom_line(data = datP[!is.na(datP$Flow.y),], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_line(data = datP[!is.na(datP$Flow.x),], aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), size = 1, linetype = "dotdash") +
           geom_line(data = datP[!is.na(datP$Estimated),], aes(x = dateTime, y = Estimated, color = "Estimated"), size = 1, linetype = "longdash") +
           geom_line(data = datP[!is.na(datP$Estimated),], aes(x = dateTime, y = Smoothed, color = "Smoothed"), size = 1, linetype = "longdash") +
           geom_line(data = datP[!is.na(datP$fitUpper),], aes(x = dateTime, y = fitUpper), color = "black", size = 1, linetype = "dashed") +
           geom_line(data = datP[!is.na(datP$fitLower),], aes(x = dateTime, y = fitLower), color = "black", size = 1, linetype = "dashed") +
+          geom_point(data = ObsDf, aes(x = dateTime, y = Flow.obs), alpha = 1, fill = "red", pch = 21, size = 3) +
           scale_y_continuous(labels = comma) +
           labs(x = "Date (UTC)", y = "Discharge, in cubic feet per second") +
+          scale_color_manual(values = c(paste0("y-", input$yID) = "blue", 
+                                        paste0("x-", input$xID) = "green",
+                                        "Smoothed" = "orange",
+                                        "Estimated" = "red")) +
           theme_bw() + theme(legend.title = element_blank())
         
       }
@@ -119,18 +145,23 @@ output$plotAllDat <- renderPlot({
       else if (input$log10 == TRUE) {
         
         p <- ggplot() +
+          geom_line(data = datP[!is.na(datP$Flow.y),], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_rect(aes(xmin = as.POSIXct(input$estDateSt, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         xmax = as.POSIXct(input$estDateEn, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         ymin = 0, ymax = Inf), alpha = 0.4, color = "grey80") +
-          geom_line(data = datP[!is.na(datP$Flow.y),], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_line(data = datP[!is.na(datP$Flow.x),], aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), size = 1, linetype = "dotdash") +
           geom_line(data = datP[!is.na(datP$Estimated),], aes(x = dateTime, y = Estimated, color = "Estimated"), size = 1, linetype = "longdash") +
           geom_line(data = datP[!is.na(datP$Estimated),], aes(x = dateTime, y = Smoothed, color = "Smoothed"), size = 1, linetype = "longdash") +
           geom_line(data = datP[!is.na(datP$fitUpper),], aes(x = dateTime, y = fitUpper), color = "black", size = 1, linetype = "dashed") +
           geom_line(data = datP[!is.na(datP$fitLower),], aes(x = dateTime, y = fitLower), color = "black", size = 1, linetype = "dashed") +
+          geom_point(data = ObsDf, aes(x = dateTime, y = Flow.obs), alpha = 1, fill = "red", pch = 21, size = 3) +
           scale_y_log10(labels = comma) +
           annotation_logticks(sides = "rl") +
           labs(x = "Date (UTC)", y = "Discharge, in cubic feet per second") +
+          scale_color_manual(values = c(paste0("y-", input$yID) = "blue", 
+                                        paste0("x-", input$xID) = "green",
+                                        "Smoothed" = "orange",
+                                        "Estimated" = "red")) +
           theme_bw() + theme(legend.title = element_blank(), panel.grid.minor = element_blank())
         
       }
@@ -140,16 +171,19 @@ output$plotAllDat <- renderPlot({
       if (input$log10 == FALSE) {
         
         p <- ggplot() +
+          geom_line(data = datP[!is.na(datP$Flow.y),], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_rect(aes(xmin = as.POSIXct(input$estDateSt, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         xmax = as.POSIXct(input$estDateEn, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         ymin = -Inf, ymax = Inf), alpha = 0.4, color = "grey80") +
-          geom_line(data = datP[!is.na(datP$Flow.y),], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_line(data = datP[!is.na(datP$Flow.x),], aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), size = 1, linetype = "dotdash") +
           geom_line(data = datP[!is.na(datP$Estimated),], aes(x = dateTime, y = Estimated, color = "Estimated"), size = 1, linetype = "longdash") +
           geom_line(data = datP[!is.na(datP$fitUpper),], aes(x = dateTime, y = fitUpper), color = "black", size = 1, linetype = "dashed") +
           geom_line(data = datP[!is.na(datP$fitLower),], aes(x = dateTime, y = fitLower), color = "black", size = 1, linetype = "dashed") +
           scale_y_continuous(labels = comma) +
           labs(x = "Date (UTC)", y = "Discharge, in cubic feet per second") +
+          scale_color_manual(values = c(paste0("y-", input$yID) = "blue", 
+                                        paste0("x-", input$xID) = "green",
+                                        "Estimated" = "red")) +
           theme_bw() + theme(legend.title = element_blank())
         
       }
@@ -157,10 +191,10 @@ output$plotAllDat <- renderPlot({
       else if (input$log10 == TRUE) {
         
         p <- ggplot() +
+          geom_line(data = datP[!is.na(datP$Flow.y),], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_rect(aes(xmin = as.POSIXct(input$estDateSt, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         xmax = as.POSIXct(input$estDateEn, format = "%Y-%m-%d %H:%M:%S", tz = "GMT"), 
                         ymin = 0, ymax = Inf), alpha = 0.4, color = "grey80") +
-          geom_line(data = datP[!is.na(datP$Flow.y),], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
           geom_line(data = datP[!is.na(datP$Flow.x),], aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), size = 1, linetype = "dotdash") +
           geom_line(data = datP[!is.na(datP$Estimated),], aes(x = dateTime, y = Estimated, color = "Estimated"), size = 1, linetype = "longdash") +
           geom_line(data = datP[!is.na(datP$fitUpper),], aes(x = dateTime, y = fitUpper), color = "black", size = 1, linetype = "dashed") +
@@ -168,6 +202,9 @@ output$plotAllDat <- renderPlot({
           scale_y_log10(labels = comma) +
           annotation_logticks(sides = "rl") +
           labs(x = "Date (UTC)", y = "Discharge, in cubic feet per second") +
+          scale_color_manual(values = c(paste0("y-", input$yID) = "blue", 
+                                        paste0("x-", input$xID) = "green",
+                                        "Estimated" = "red")) +
           theme_bw() + theme(legend.title = element_blank(), panel.grid.minor = element_blank())
         
       }
--- inst/shiny/plotComp1.R
+++ inst/shiny/plotComp1.R
@@ -16,6 +16,8 @@ output$plotComp1 <- renderPlot({
         geom_line(data = regDat[!is.na(regDat$Flow.x),], aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), size = 1, linetype = "dotdash") +
         labs(x = "Date (UTC)", y = "Discharge, in cubic feet per second",
              title = paste0("calculated best time offset = ", bestOffsetVal, " minutes")) +
+        scale_color_manual(values = c(paste0("y-", input$yID) = "blue", 
+                                      paste0("x-", input$xID) = "green")) +
         theme_bw() + theme(legend.title = element_blank())
       
     }
@@ -32,6 +34,8 @@ output$plotComp1 <- renderPlot({
         annotation_logticks(sides = "rl") +
         labs(x = "Date (UTC)", y = "Discharge, in cubic feet per second", 
              title = paste0("calculated best time offset = ", bestOffsetVal, " minutes")) +
+        scale_color_manual(values = c(paste0("y-", input$yID) = "blue", 
+                                      paste0("x-", input$xID) = "green")) +
         theme_bw() + theme(legend.title = element_blank(), panel.grid.minor = element_blank())
       
     }
@@ -51,6 +55,8 @@ output$plotComp1 <- renderPlot({
         geom_line(data = regDat[!is.na(regDat$Flow.y),], aes(x = dateTime, y = Flow.y, color = paste0("y-", input$yID)), size = 1) +
         geom_line(data = regDat[!is.na(regDat$Flow.x),], aes(x = dateTime, y = Flow.x, color = paste0("x-", input$xID)), size = 1, linetype = "dotdash") +
         labs(x = "Date (UTC)", y = "Discharge, in cubic feet per second") +
+        scale_color_manual(values = c(paste0("y-", input$yID) = "blue", 
+                                      paste0("x-", input$xID) = "green")) +
         theme_bw() + theme(legend.title = element_blank())
       
     }
@@ -66,6 +72,8 @@ output$plotComp1 <- renderPlot({
         scale_y_log10() +
         annotation_logticks(sides = "rl") +
         labs(x = "Date (UTC)", y = "Discharge, in cubic feet per second") +
+        scale_color_manual(values = c(paste0("y-", input$yID) = "blue", 
+                                      paste0("x-", input$xID) = "green")) +
         theme_bw() + theme(legend.title = element_blank(), panel.grid.minor = element_blank())
       
     }
